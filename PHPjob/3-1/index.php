31550293ba0286ac514466a94542815d19c2cf5e
takuya-89 <103874385+takuya-89@users.noreply.github.com>
2022-07-08 23:24:23 +0900

    3-1課題提出

---
+++ PHPjob/3-1/index.php
@@ -0,0 +1,15 @@
+<?php
+for($num = 1 ; $num <= 100 ; $num++){
+  if($num % 15 == 0){
+    echo "FizzBuzz!";
+   }elseif($num % 3 == 0){
+    echo "Fizz!";
+   }elseif($num % 5 == 0){
+    echo "Buzz!";
+   }else{
+    echo $num;
+   }
+   echo "<br>";
+}
+
+?>
