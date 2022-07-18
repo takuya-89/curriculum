e2e0ae8f049b9a2683ab923945e4363f13265c19
takuya-89 <103874385+takuya-89@users.noreply.github.com>
2022-07-18 21:36:46 +0900

    3-1課題提出(修正)

--- PHPjob/3-1/index.php
+++ PHPjob/3-1/index.php
@@ -1,11 +1,11 @@
 <?php
 for($num = 1 ; $num <= 100 ; $num++){
-  if($num % 15 == 0){
+  if($num % 3 == 0 && $num % 5 == 0){
     echo "FizzBuzz!";
-   }elseif($num % 3 == 0){
-    echo "Fizz!";
    }elseif($num % 5 == 0){
     echo "Buzz!";
+   }elseif($num % 3 == 0){
+    echo "Fizz!";
    }else{
     echo $num;
    }
